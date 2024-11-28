cb05c6538d7f3dab609d1c76d0e4c2c45bef3b25
hebertl <lena.hebert@edu.esiee.fr>
2024-11-28 10:36:30 +0000

    creation de la fonction isprime()

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+""" programme prime"""
 from math import sqrt
+
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
+    """fonction pour determiner si un nombre est premier"""
     # votre code ici
-
-    pass
+    premier = True
+    if p==1:
+        premier =False
+    for diviseur in range(2,int(sqrt(p))+1) :
+        if p % diviseur == 0 :
+            premier = False
+            break
+    return premier
 
 #### Fonction principale
 
 
 def main():
-
+    """fonction main"""
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
